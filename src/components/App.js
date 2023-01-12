38f4066679f98383ec303c1872ef14e2847b07b6
joy <letimjoy7@gmail.com>
2023-01-12 18:31:33 +0300

    done

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,5 @@
 import React from "react";
-
-function NavBar() {
+function Navbar() {
   return (
     <nav>
       <a href="#home">I'm a link!</a>
@@ -16,17 +15,23 @@ function Home() {
   );
 }
 
-{/* write an <About> component here */}
+function About(){
+  return(
+    <div id="about">
+      <h2>About</h2>
+    </div>
+  );
+}
 
 
 function App() {
   return (
     <div>
-      <NavBar />
-      {/* add the <Home> component here */}
-      {/* add your <About> component here */}
+      <Navbar />
+      <Home />
+      <About />
     </div>
   );
 }
 
-export default App;
+export default App
---
+++ src/components/Navbar.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+function Navbar() {
+  return (
+    <nav>
+      <a href="#home">I'm a link!</a>
+    </nav>
+  );
+}
+export default Navbar
